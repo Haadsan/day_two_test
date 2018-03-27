d2ca44564aedb8861390f863abcc3f7ce59f545b
Edward Fallon <eddie_fallon@hotmail.com>
2018-03-27 15:28:35 +0100

    number_to_full_month_name added

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -25,6 +25,19 @@ end
 def join_string(string_1, string_2)
   return string_1 + string_2
 end
+
 def add_string_as_number(string_1, string_2)
   return string_1.to_i + string_2.to_i
+end
+
+def number_to_full_month_name(month_number)
+  month_string = case month_number
+  when 1
+    month_string = "January"
+  when 3
+    month_string = "March"
+  when 9
+    month_string = "September"
+  end
+  return month_string
 end
